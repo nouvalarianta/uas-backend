b294537a9de183585250dc6fb2311e930e7b8d59
nouval <nouvalarianta@gmail.com>
2025-12-19 15:19:13 +0700

    change helper

--- helper/helper.go
+++ helper/helper.go
@@ -1,9 +1,6 @@
 package helper
 
 import (
-	// "fmt"
-
-	// "github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -27,26 +24,3 @@ func ErrorResponse(c *fiber.Ctx, code int, message string) error {
 		"message": message,
 	})
 }
-
-// func ValidationError(c *fiber.Ctx, errors interface{}) error {
-// 	return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-// 		"success": false,
-// 		"message": "Request body tidak valid",
-// 		"errors":  errors,
-// 	})
-// }
-
-// // validator
-// func FormatValidationErrors(err error) []ValidationErrorResponse {
-// 	var errors []ValidationErrorResponse
-
-// 	if validationErrors, ok := err.(validator.ValidationErrors); ok {
-// 		for _, fieldErr := range validationErrors {
-// 			errors = append(errors, ValidationErrorResponse{
-// 				Field:   fieldErr.Field(),
-// 				Message: fmt.Sprintf("Aturan validasi '%s' gagal", fieldErr.Tag()),
-// 			})
-// 		}
-// 	}
-// 	return errors
-// }
